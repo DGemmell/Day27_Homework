578ab57079829053719c628ecd6a64cc8801aca8
DGemmell <debbiecarden@hotmail.co.uk>
2018-02-28 15:10:08 +0000

    Adding Tuesdays homework

--- TestLibrary.java
+++ TestLibrary.java
@@ -1,6 +1,5 @@
 import org.junit.Before;
 import org.junit.Test;
-import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 
 public class TestLibrary {
@@ -10,33 +9,35 @@ public class TestLibrary {
 
     @Before
     public void before(){
-        ArrayList<Book> books = new ArrayList<Book>();
-        library = new Library(books, 1000);
+        library = new Library(1000, "Cathcart Library");
+        book = new Book("Stick Man", "Julia Donaldson");
 
     };
 
     @Test
     public void canGetStockCapacity(){
-        assertEquals(1000, Library.getStockCapacity());
+        assertEquals(1000, library.getStockCapacity());
     };
 
     @Test
     public void canAddBook(){
         library.addBook(book);
-        assertEquals(1001, Library.getStockCapacity());
+        assertEquals(1, library.getNumberOfBooks());
     };
 
 //    ok this one i really wasn't sure so i referred to the notes for the bus and remembered Aly
 //    talking about i++ yesterday. im not sure this will work however, I would like to write
 //    1.measure current capacity then if capacity is <1002 then add a book.  I have added the book from the
 //    Library class but wondered if this should be the Book class?
+
     @Test
     public void measureCapacity() {
         int i;
-        for (i = 0; i <1002,i++){
+        for (i = 0; i <1002; i++)
+        {;
             library.addBook(book);
         }
-        assertEquals(1001, library.getStockCapacity() );
+        assertEquals(1000, library.getStockCapacity());
     }
 
 
